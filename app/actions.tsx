3876340aa537fdb59a08e663f2245a7676fde1df
aradhyasuggaa <aradhya@suggaa.in>
2024-06-17 19:13:18 +0530

    commit without response_type

---
+++ app/actions.tsx
@@ -0,0 +1,163 @@
+'use server';
+
+import { generateId } from 'ai';
+import { createAI, createStreamableUI, createStreamableValue } from 'ai/rsc';
+import { OpenAI } from 'openai';
+import { ReactNode } from 'react';
+import { getWeatherReport } from './getWeatherReport';
+import { Message } from './message';
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+export interface ClientMessage {
+  id: string;
+  status: ReactNode;
+  text: ReactNode;
+  gui: ReactNode;
+}
+
+const ASSISTANT_ID = process.env.OPENAI_ASSISTANT_KEY as string;
+let THREAD_ID = '';
+let RUN_ID = '';
+
+export async function submitMessage(question: string): Promise<ClientMessage> {
+  const status = createStreamableUI('thread.init');
+  const textStream = createStreamableValue('');
+  const textUIStream = createStreamableUI(
+    <Message textStream={textStream.value} />,
+  );
+  const gui = createStreamableUI();
+
+  const runQueue = [];
+
+  (async () => {
+    if (THREAD_ID) {
+      await openai.beta.threads.messages.create(THREAD_ID, {
+        role: 'user',
+        content: question,
+      });
+
+      const run = await openai.beta.threads.runs.create(THREAD_ID, {
+        assistant_id: ASSISTANT_ID,
+        
+
+        stream: true,
+      });
+
+      runQueue.push({ id: generateId(), run });
+    } else {
+      const run = await openai.beta.threads.createAndRun({
+        assistant_id: ASSISTANT_ID,
+
+        stream: true,
+        thread:{
+          messages: [{ role: 'user', content: question }] ,
+        },
+      });
+
+      runQueue.push({ id: generateId(), run });
+    }
+
+    while (runQueue.length > 0) {
+      const latestRun = runQueue.shift();
+
+      if (latestRun) {
+        for await (const delta of latestRun.run) {
+          const { data, event } = delta;
+
+          status.update(event);
+
+          if (event === 'thread.created') {
+            THREAD_ID = data.id;
+          } else if (event === 'thread.run.created') {
+            RUN_ID = data.id;
+          } else if (event === 'thread.message.delta') {
+            data.delta.content?.map((part: any) => {
+              if (part.type === 'text') {
+                if (part.text) {
+                  textStream.append(part.text.value);
+                }
+              }
+            });
+          } else if (event === 'thread.run.requires_action') {
+            if (data.required_action) {
+              if (data.required_action.type === 'submit_tool_outputs') {
+                const { tool_calls } = data.required_action.submit_tool_outputs;
+                const tool_outputs = [];
+
+                for (const tool_call of tool_calls) {
+                  const { id: toolCallId, function: fn } = tool_call;
+                  const { name, arguments: args } = fn;
+
+                  if (name === 'get_weather_report') {
+                    const { location } = JSON.parse(args);
+
+                    gui.append(
+                      <div className="flex flex-row gap-2 items-center">
+                        <div>
+                          Fetching weather report for: {location}
+                        </div>
+                      </div>,
+                    );
+
+                    await new Promise(resolve => setTimeout(resolve, 2000));
+
+                    const weatherReport = await getWeatherReport(location);
+
+                    gui.append(
+                      <div className="flex flex-col gap-2">
+                        <div className="p-2 bg-zinc-100 rounded-md flex flex-row gap-2 items-center justify-between">
+                          <div className="flex flex-row gap-2 items-center">
+                            <div>Location: {weatherReport.location.name}</div>
+                            <div>Temperature: {weatherReport.current.temp_c}°C</div>
+                            <div>Condition: {weatherReport.current.condition.text}</div>
+                          </div>
+                        </div>
+                      </div>,
+                    );
+
+                    tool_outputs.push({
+                      tool_call_id: toolCallId,
+                      output: JSON.stringify(weatherReport),
+                    });
+                  }
+                }
+
+                const nextRun: any =
+                  await openai.beta.threads.runs.submitToolOutputs(
+                    THREAD_ID,
+                    RUN_ID,
+                    {
+                      tool_outputs,
+                      stream: true,
+                    },
+                  );
+
+                runQueue.push({ id: generateId(), run: nextRun });
+              }
+            }
+          } else if (event === 'thread.run.failed') {
+            console.log(data);
+          }
+        }
+      }
+    }
+
+    status.done();
+    textUIStream.done();
+    gui.done();
+  })();
+
+  return {
+    id: generateId(),
+    status: status.value,
+    text: textUIStream.value,
+    gui: gui.value,
+  };
+}
+
+export const AI = createAI({
+  actions: { submitMessage },
+});
---
+++ app/getWeatherReport.tsx
@@ -0,0 +1,7 @@
+import axios from 'axios';
+
+export async function getWeatherReport(location: string): Promise<any> {
+  const apiKey = process.env.OPENAI_API_KEY;
+  const response = await axios.get(`https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${location}`);
+  return response.data;
+}
